1d893ea3e09429a111697ee68cc3db64d073d147
thedevaman <devaman2508@gmail.com>
2026-03-11 09:31:28 +0530

    having defrrent model and schema file

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-import mongoose from 'mongoose'
-async function dbConnection(){
-    await mongoose.connect('mongodb://localhost:27017/college')
-    const schema = mongoose.Schema({
-        name:String,
-        age:Number,
-        email:String
+// import mongoose from 'mongoose'
+// async function dbConnection(){
+//     await mongoose.connect('mongodb://localhost:27017/college')
+//     const schema = mongoose.Schema({
+//         name:String,
+//         age:Number,
+//         email:String
+//     })
+
+//     const studentsModel = mongoose.model('students',schema);
+//     const result = await studentsModel.find();
+//     console.log(result)
+// }
+
+// dbConnection()
+
+
+import mongoose from "mongoose";
+import express from 'express'
+import studentModel from "./model/studentModel.js";
+
+const app = express()
+
+ await mongoose.connect('mongodb://localhost:27017/college').then(()=>{
+    console.log("connected")
+ })
+
+app.get("/",async(req,res)=>{
+
+    const studentData = await studentModel.find()
+
+    res.send({
+      studentData
     })
 
-    const studentsModel = mongoose.model('students',schema);
-    const result = await studentsModel.find();
-    console.log(result)
-}
+})
 
-dbConnection()
+app.listen(3200)
